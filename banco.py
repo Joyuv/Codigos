325ca3d7c3f07a6035ae3c9d2b91669f276231d9
Joyuv <givanilsonmariz7@outlook.com>
2023-04-01 18:45:39 -0300

    exercicio2

---
+++ banco.py
@@ -0,0 +1,5 @@
+print('Olá caro cliente insira seu nome para continuar\n')
+
+nome = input('Nome')
+
+print('Olá', nome, 'seja bem vindo ao Banco Py\nQue tipo de operação desejas fazer?')
---
+++ exercicio2.py
@@ -0,0 +1,3 @@
+nome = input('Qual é o seu nome?\n')
+
+print('Prazer em te conhecer', nome, 'o meu nome é Giva o criador desse código.')
