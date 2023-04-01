d8f7e46a1eb3ebe553947024d28852eb06f22229
Joyuv <givanilsonmariz7@outlook.com>
2023-04-01 20:12:34 -0300

    exercicio3

---
+++ exercício3.py
@@ -0,0 +1,13 @@
+a = input('Escreva algo: ')
+
+print(type(a))
+print(('alfanumérico '), a.isalnum())
+print(('ascii'), a.isascii())
+print(('decimal'), a.isdecimal())
+print(('digito'), a.isdigit())
+print(('identificador'), a.isidentifier())
+print(('minúscula'), a.islower())
+print(('imprimível'), a.isprintable())
+print(('espaço'), a.isspace())
+print(('título'), a.istitle())
+print(('maiuscula'), a.isupper())
