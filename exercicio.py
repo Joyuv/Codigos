f29dd49be3f537948a498062310d1580195e8ab1
Joyuv <givanilsonmariz7@outlook.com>
2023-04-01 19:45:17 -0300

    atualizacao2

--- exercicio.py
+++ exercicio.py
@@ -1,9 +1,9 @@
 print('olá como vai você neste lindo dia? sabes somar?')
 
-num1 = int(input('Primeiro número'))
+num1 = int(input('Primeiro número: '))
 
-num2 = int(input('Segundo número'))
+num2 = int(input('Segundo número: '))
 
 soma = num1 + num2
 
-print('A soma entre', num1, 'e', num2, 'é', soma)
+print('A soma entre {} e {} é {}'.format(num1, num2, soma))
