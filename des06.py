8cd2e5fe314290e14ba3b5cdf09d2aa07fa94671
Joyuv <givanilsonmariz7@outlook.com>
2023-04-05 18:52:20 -0300

    aula07

---
+++ des06.py
@@ -0,0 +1,6 @@
+#obj: pedir um número mostrar o dobr triplo e raiz quadrada
+n = int(input('digite um número inteiro: '))
+d = n * 2
+t = n * 3
+r = n ** (1/2)
+print('o dobro de {} é {} o triplo é {} e a raiz quadrada é {}'.format(n, d, t, r))
