5f72d7a547e5feaadd74d6950368410c32cd6260
Joyuv <givanilsonmariz7@outlook.com>
2023-04-05 18:46:54 -0300

    aula07

---
+++ des05.py
@@ -0,0 +1,6 @@
+#obj: pedir um número inteiro e mostrar o sucessor e o antecessor
+
+n = int(input('digite um número inteiro'))
+s = n + 1
+a = n - 1
+print('o antecessor de {} digitado é {} e o sucessor é {}'.format(n, a, s))
---
+++ pytest/aula07test.py
@@ -0,0 +1,8 @@
+n1 = int(input('digite um número'))
+n2 = int(input('digite outro número'))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+p = n1 ** n2
+print('os resultados são: soma = {} multiplicação = {} divisão = {:.2f} divisão igual = {} potência = {}'.format(s, m, d, di, p))
