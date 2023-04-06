a2f32b8863e9b51699321a042398a1e7e26f6851
Joyuv <givanilsonmariz7@outlook.com>
2023-04-06 16:51:52 -0300

    des09

---
+++ des09.py
@@ -0,0 +1,3 @@
+n = int(input('digite um valor: '))
+
+print('a tabuada de {} é\n1 = {}\n2 = {}\n3 = {}\n4 = {}\n5 = {}\n6 = {}\n7 = {}\n8 = {}\n9 = {}\n10 = {}'.format(n, n * 1, n * 2, n * 3, n * 4, n * 5, n * 6, n * 7, n * 8, n * 9, n * 10))
