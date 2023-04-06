6d07bb7f42e1e5245eebbd657f55d1fb538b926a
Joyuv <givanilsonmariz7@outlook.com>
2023-04-06 17:14:19 -0300

    aula07

---
+++ des11.py
@@ -0,0 +1,7 @@
+a = int(input('insira a altura de sua parede: '))
+l = int(input('insira largura de sua parede: '))
+
+ar = a * l
+t = ar / 2
+
+print('a area da sua parede é {} m quadrados, a quantidade de tinta que você vai precisar é {} l'.format(ar, t))
