56a323539ef14866fba88582b24f195d5d49646e
Joyuv <givanilsonmariz7@outlook.com>
2023-04-06 16:30:00 -0300

    des08

--- des08.py
+++ des08.py
@@ -2,4 +2,6 @@ m = float(input('digite um valor em metros: '))
 
 c = m * 10 ** 2
 
-print('o valor de metros digitados em centimetros é {}'.format(c))
+mm = m * 10 ** 3
+
+print('o valor de metros digitados em centimetros é {} e em milimetros é {}'.format(c, mm))
