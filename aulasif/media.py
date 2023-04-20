53869378d68454659f987196f695091832604b67
Joyuv <givanilsonmariz7@outlook.com>
2023-04-20 11:17:43 -0300

    upd

--- aulasif/media.py
+++ aulasif/media.py
@@ -2,27 +2,15 @@
 
 e = int(input('insira o número de notas para calcular a média 4/2'))
 
-if (e == 2):
+print('Programa que calcula média.')
 
-    print('Programa que calcula média.')
+if (e == 2):
 
     v1 = int(input('\n insira o primeiro valor: '))
 
     v2 = int(input('\n insira o segundo valor: '))
 
-    m = (v1 + v2) / 2
-
-    if (m >= 60):
-
-        print('\na sua média é {} parabéns você passou '.format(m))
-
-    elif (m):
-
-        print('sua média é {} que pena você não passou'.format(m))
-
 if (e == 4):
-
-    print('Programa que calcula média.')
 
     v1 = int(input('\n insira o primeiro valor: '))
 
@@ -32,12 +20,19 @@ if (e == 4):
 
     v4 = int(input('\n insira o quarto valor: '))
 
+
+if (e == 2):
+
+    m = (v1 + v2) / 2
+
+if (e == 4):
+
     m = (v1 + v2 + v3 + v4) / 4
 
-    if (m >= 60):
+if (m >= 60):
 
-        print('\na sua média é {} parabéns você passou '.format(m))
+    print('\na sua média é {} parabéns você passou '.format(m))
 
-    elif (m):
+elif (m):
 
-        print('sua média é {} que pena você não passou'.format(m))
+    print('sua média é {} que pena você não passou'.format(m))
