cb2f6a422e3acc6f4d4d42a3171ff3eb94ecf40f
Joyuv <givanilsonmariz7@outlook.com>
2023-05-17 14:15:42 -0300

    desafios

---
+++ php/des05.php
@@ -0,0 +1,9 @@
+<?php
+
+$n = readline("insira um número e receba seu sucessor e antecessor: ");
+
+$s = $n + 1;
+
+$a = $n - 1;
+
+echo("Seu antecessor é ". $a. " e seu sucessor é ". $s);
---
+++ php/des06.php
@@ -0,0 +1,11 @@
+<?php
+
+$n = $argv[1];
+
+$d = $n * 2;
+
+$t = $n * 3;
+
+$rq = sqrt($n);
+
+echo ("seu dobro é ". $d. " seu triplo é ". $t. " sua raiz é ". $rq);
