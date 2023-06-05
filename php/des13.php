069a77be2845d92facc4540a95cf6bb5bd80e2fa
Joyuv <givanilsonmariz7@outlook.com>
2023-06-05 10:19:45 -0300

    des13php

---
+++ php/des13.php
@@ -0,0 +1,10 @@
+<?php
+
+$s = readline("insira o valor do seu salário: ");
+
+$a = $s * (15/100);
+
+$ns = $s + $a;
+
+echo ("seu novo salário é de ". $ns. "R$ boa sorte ai ganhando mais dinheiro.");
+
