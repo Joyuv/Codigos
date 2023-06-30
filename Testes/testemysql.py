e17ce9d079eeb6ac4eac6919d25f376171ec86c9
Joyuv <givanilsonmariz7@outlook.com>
2023-06-30 20:43:39 -0300

    mysql

--- Testes/testemysql.py
+++ Testes/testemysql.py
@@ -10,9 +10,10 @@ cadastro = mysql.connector.connect(
 name =  input('insira seu nome: ')
 namev = name.split()
 c = cadastro.cursor()
+c.execute ('create table if not exists teste( nome varchar (30) NOT NULL)')
 add_user = ('insert into teste'
-            '(nome)'
-            'values(%s)')
+    '(nome)'
+    'values(%s)')
 data_user = (namev)
 c.execute(add_user, data_user)
 cadastro.commit()
