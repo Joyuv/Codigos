dd3a0e3e59d83c5f7c3e6370a373751bcbf9e2f8
Joyuv <givanilsonmariz7@outlook.com>
2023-07-20 11:00:29 -0300

    atsalawhileforlist

---
+++ aulasif/atwhileforlist.py
@@ -0,0 +1,22 @@
+lista = []
+soma = 0
+ct = 0
+
+while True:
+    nome = input('Insira um nome ae: ')
+    if nome != 'parar':
+        altura = float(input('insira tua altura ae: '))
+        soma = altura + soma
+        ct +=1
+        alturas = str(altura)
+        nome = nome + ' '
+        nomea = nome + alturas
+
+        lista.append (nomea)
+    if nome == 'parar':
+        break
+    
+med = soma/ct
+for nomes in lista:
+    print (lista)
+print (f'A média das alturas é {med}')
