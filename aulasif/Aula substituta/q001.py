6c8f80f5f1603450e8d38b704d0888affe495de9
Joyuv <givanilsonmariz7@outlook.com>
2023-09-27 11:23:37 -0300

    Aulasubstituta

---
+++ aulasif/Aula substituta/q001.py
@@ -0,0 +1,13 @@
+va = [1,3,5,7,9]
+vb = [2,4,6,8,10]
+
+vc = []
+
+for x in range (0,5):
+
+    vc.append(va[x])
+    vc.append(vb[x])
+
+for x in vc:
+    print(x)
+    
---
+++ aulasif/Aula substituta/q002.py
@@ -0,0 +1,14 @@
+lista1, lista2, soma = [], [], 0
+
+for x in range (0,5):
+
+    lista1.append(int(input('Insira o número da primeira lista: ')))
+    lista2.append(int(input('Insira o número da segunda lista: ')))
+
+for x in range (0,5):
+
+    soma = (lista1[x] * lista2[x]) + soma
+
+print(lista1)
+print(lista2)
+print(soma)
