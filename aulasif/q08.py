e0a1806f3a19acbad5cf7ffef78e0c8ec62da962
Joyuv <givanilsonmariz7@outlook.com>
2023-10-19 11:02:18 -0300

    aua

--- aulasif/q08.py
+++ aulasif/q08.py
@@ -7,4 +7,13 @@ snome = input('insira o seu segundo nome a seguir: ')
 list2 = []
 for x in snome:
     list2.append(x)
-print(f'O seu primeiro nome possui {len(list1)} letras o segundo tem {len(list2)} e o seu nome completo é {pnome} {snome}')
+
+def nome_completo(l1,l2):
+    nomecompleto = ''
+    for x in l1:
+        nomecompleto = nomecompleto + x
+    nomecompleto += ' '
+    for x in l2:
+        nomecompleto = nomecompleto + x
+    return nomecompleto
+print(f'O seu primeiro nome possui {len(list1)} letras o segundo tem {len(list2)} e o seu nome completo é {nome_completo(list1, list2)}')
