7e31df8638d92b6fda073a0eddc9ff52188cb6f0
Joyuv <givanilsonmariz7@gmail.com>
2023-10-19 14:06:55 -0300

    19/10

---
+++ ideiasproprias/jogos/conceito.py
@@ -0,0 +1,33 @@
+# class player():
+
+#     def __init__(self, nome, vida, defesa, ataque, exp)
+
+#         self.__nome = nome
+#         self.__vida = vida
+#         self.__defesa = defesa
+#         self.__ataque = ataque
+#         self.__exp = exp
+
+#     def get_vida(self):
+#         return self.__vida
+#     def get_defesa(self):
+#         return self.__defesa
+#     def get_ataque(self):
+#         return self.__ataque
+#     def get_exp(self):
+#         return self.__ataque
+#     def get_nome(self):
+#         return self.__nome
+    
+#     def set_vida(self, vida):
+#         self.__vida = vida
+#     def set_defesa(self, defesa):
+#         self.__defesa = defesa
+#     def set_ataque(self, ataque):
+#         self.__ataque = ataque
+#     def set_exp(self, exp):
+#         self.__exp = exp
+
+# nome = input('Insira o nome de seu personagem a seguir: ')
+
+# jogador = player()
