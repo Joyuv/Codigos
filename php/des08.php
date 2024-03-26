4e8ea6b5ae3e07cf0c5a4f8e9e539604525eb1f4
Joyuv <givanilsonmariz7@outlook.com>
2024-03-26 11:12:29 -0300

    revisaophp

--- php/des08.php
+++ php/des08.php
@@ -1,9 +1,11 @@
 <?php
 
+echo ("\n");
+
 $n = $argv[1];
 
 $c = pow(10, 2) * $n;
 
 $mm = pow(10, 3) * $n;
 
-echo ("o valor de ". $n. "m em cm é ". $c. " e em mm é ". $mm);
+echo ("o valor de ". $n. "m em cm é ". $c. " e em mm é ". $mm. "\n\n");
