2cb6b05c7a2a9ddf34d4ef6ba407c508773cb795
Joyuv <givanilsonmariz7@outlook.com>
2024-07-14 18:54:40 -0300

    revhtml

--- Testes/Letra-a-letra.py
+++ Testes/Letra-a-letra.py
@@ -6,4 +6,4 @@ def delprint(text,delay_time):
     sys.stdout.flush()
     time.sleep(delay_time)
 
-delprint('Eu sou um texto de teste, não se preocupe comigo.\n', 0.04)
+delprint('Eu sou um texto de teste, não se preocupe comigo.\n', 0.08)
